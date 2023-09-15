32836e1ec673400cefcb8d8975299d64aa72519f
Hayomide1 <hayomide_a@AYOMIDE-PC.localdomain>
2023-09-15 23:42:57 +0100

    he

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -6,31 +6,28 @@
  * followed by a new line
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-	long int n;
-	long int max;
-	long int i;
-	
-	n = 612852475143;
-	max = -1;
-	
-	while (n % 2 == 0)
+	long number = 612852475143;
+	int inc;
+
+	while (inc++ < number / 2)
 	{
-		max = 2;
-		n /= 2;
-	}
-	for (i = 3; i <= sqrt(n); i = i + 2)
-	{
-		while (n % i == 0)
+		if (number % inc == 0)
 		{
-			max = i;
-			n = n / i;
+			number /= 2;
+			continue;
+		}
+
+		for (inc = 3; inc < number / 2; inc += 2)
+		{
+			if (number % inc == 0)
+				number /= inc;
+
 		}
 	}
-	if (n > 2)
-		max = n;
-	printf("%ld\n", max);
+	printf("%ld\n", number);
 	return (0);
 }
 
