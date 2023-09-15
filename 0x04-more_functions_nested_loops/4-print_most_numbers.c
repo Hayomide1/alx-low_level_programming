db6efa19894545a403ec82aed049f1009f9eef0e
Hayomide1 <hayomide_a@AYOMIDE-PC.localdomain>
2023-09-15 23:25:01 +0100

    he

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,13 +8,16 @@
 
 void print_most_numbers(void)
 {
-	char c;
-	
-	for (c = '0'; c <= '9'; c++)
+	int number;
+
+	for (number = 48; number < 58; number++)
 	{
-		if (!(c == '2' || c == '4'))
-			_putchar(c);
+		if ((number == 50) || (number == 52))
+		{
+			continue;
+		}
+		_putchar(number);
 	}
-	_putchar('\n');
+	_putchar(10);
 }
 
