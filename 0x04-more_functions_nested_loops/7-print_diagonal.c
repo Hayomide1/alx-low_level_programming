7d6c8078404b5079dda475b7cefa89f1dbbc5050
Hayomide1 <hayomide_a@AYOMIDE-PC.localdomain>
2023-09-15 23:31:36 +0100

    he

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -7,23 +7,22 @@
 
 void print_diagonal(int n)
 {
-	if (n <= 0)
+	int len, space;
+
+	if (n > 0)
 	{
-		_putchar('\n');
-	} else
-	{
-		int i, j;
-		for (i = 0; i < n; i++)
+		for (len = 0; len < n; len++)
 		{
-			for (j = 0; j < n; j++)
-			{
-				if (j == i)
-					_putchar('\\');
-				else if (j < i)
-					_putchar(' ');
-			}
+			for (space = 0; space < len; space++)
+				_putchar(' ');
+
+			_putchar('\\');
+
+			if (len == (n - 1))
+				continue;
 			_putchar('\n');
 		}
 	}
+	_putchar('\n');
 }
 
