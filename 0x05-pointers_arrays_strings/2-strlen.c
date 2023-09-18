abd3ddcd9ca2395303f58ddbd3e91ba218feb4ed
Hayomide1 <hayomide_a@AYOMIDE-PC.localdomain>
2023-09-18 20:32:44 +0100

    yop

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,15 +1,14 @@
 #include "main.h"
 
 /**
- *  * _strlen - returns the length of a string
- *   * @s: string to evaluate
- *    *
- *     * Return: the length of the string
- *      */
+ * _strlen - returns the length of a string
+ * @s: string to evaluate
+ *
+ * Return: the length of the string
+ */
 int _strlen(char *s)
 {
-	int i;
-	
+	int i;	
 	i = 0;
 	
 	while (s[i] != '\0')
